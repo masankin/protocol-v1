e1c6990b69158d8307a109ed9df27a868fc2cbd8
masankin <masankin@192.168.0.196>
2018-11-02 21:49:20 +0800

    v1

--- protocol/HallCMD.java
+++ protocol/HallCMD.java
@@ -15,20 +15,6 @@ public class HallCMD {
 		public final static int HALL_QUERY_NEWBIE = 0x15;
 	}
 
-	public class RCV {
-		public final static int HALLR_LOGIN_SUCC = 0x30;
-		public final static int HALLR_GET_ONLINE_PLAYERS_COUNT = 0x36;
-		public final static int HALLR_SYS_NEWBOARDCAST = 0x53;
-		public final static int HALLR_SYS_OLDBOARDCAST = 0x37;
-		public final static int HALLR_FRIEND_MSG = 0x41;
-		public final static int HALLR_SYS_INFO = 0x42;
-		public final static int HALLR_INVITE_FRIEND = 0x43;
-		public final static int HALLR_SERVICE_MSG = 0x44;
-		public final static int HALLR_SYNC_INFO = 0x4A;
-		public final static int HALLR_OFFLINE_MSG = 0x4C;
-		public final static int HALLR_HEARTBEAT = 0x51;
-		public final static int HALLR_FORCE_OFFLINE = 0x52;
-		public final static int HALLR_MISSION_ONCHANGE = 0x38;
-		public final static int HALLR_NEW_MAIL = 0x56; 
+	public class RCV { 
 	}
 }
